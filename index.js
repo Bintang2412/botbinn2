20fbf9c442ce1a4456f8b27d2b3768d13eabf9e2
Bintang2412 <74962335+Bintang2412@users.noreply.github.com>
2020-11-25 13:42:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'NAMA BOTMU'; 
-const instagram = 'LINK INSTAGRAMU'; 
-const telegram = 'LINK TELEMU'; 
+const BotName = 'BINN BOT'; 
+const instagram = 'https://www.instagram.com/bintang.mtrm24'; 
+const telegram = 'https://t.me/BINN24'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'LINK YTMU';
-//const grupch1 = 'belum ada grup'; 
+const youtube = 'https://www.youtube.com/channel/UC3_-1Ck1krIV1zljn-4Ql9A';
+//const grupch1 = 'https://chat.whatsapp.com/G3fXf4NumHn8aa0ii1xcVC'; 
 //const grupch2 = 'belum ada grup' ; 
 const
 {
@@ -68,7 +68,7 @@ conn.on('user-presence-update', json => console.log(json.id + ' presence is => '
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@reuz.id`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@bintang.mtrm24`)
 })
 
 conn.on('message-new', async(m) =>
